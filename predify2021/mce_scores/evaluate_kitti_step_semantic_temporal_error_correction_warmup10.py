c891889b1979ce1051bdbcfc601ef53c3a103052
zhihuilin <1404765173@qq.com>
2026-08-29 02:23:11 +0800

    Remove duplicate warmup entry points

--- predify2021/mce_scores/evaluate_kitti_step_semantic_temporal_error_correction_warmup10.py
+++
@@ -1,9 +0,0 @@
-from predify2021.mce_scores import evaluate_kitti_step_semantic_temporal_error_correction as experiment
-
-
-experiment.WARMUP_FRACTION = 0.10
-experiment.SKIP_WARMUP = True
-
-
-if __name__ == "__main__":
-    experiment.main()
