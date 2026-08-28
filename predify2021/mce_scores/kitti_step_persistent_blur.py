4bd03d911dfc496175e7005416ee7317344d880c
zhihuilin <1404765173@qq.com>
2026-08-28 18:56:05 +0800

    Use 10 percent clean warm-up for persistent blur

--- predify2021/mce_scores/kitti_step_persistent_blur.py
+++ predify2021/mce_scores/kitti_step_persistent_blur.py
@@ -5,10 +5,15 @@ import torchvision.transforms.functional as TF
 BLUR_KERNEL_SIZE = 11
 BLUR_SIGMA_MAX = 3.0
 BLUR_SIGMA_LEVELS = (0.75, 1.5, 2.25, 3.0)
+BLUR_WARMUP_FRACTION = 0.10
+
+
+def warmup_frame_count(total_frames):
+    return total_frames // 10
 
 
 def persistent_gaussian_blur(image, frame_index, total_frames):
-    onset = total_frames // 3
+    onset = warmup_frame_count(total_frames)
     if frame_index < onset:
         return image
     sigma = BLUR_SIGMA_LEVELS[min(frame_index - onset, len(BLUR_SIGMA_LEVELS) - 1)]
