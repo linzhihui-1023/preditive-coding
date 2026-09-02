70d73ac71028c3bb13c8259061b78da38a7b36d8
zhihuilin <1404765173@qq.com>
2026-09-02 14:00:31 +0800

    Use context-gated Z4 restoration predictor

--- predify2021/model_factory/deeplabv3plus_resnet50/__init__.py
+++ predify2021/model_factory/deeplabv3plus_resnet50/__init__.py
@@ -14,7 +14,6 @@ from .semantic_recurrent_predictor import (
     ErrorGuidedSemanticRecurrentCell,
     ErrorGuidedSemanticRestorationPredictor,
     ErrorRegulatedSemanticStateCell,
-    ErrorRegulatedSemanticRestorationPredictor,
     RoleSeparatedRecurrentPredictor,
     SemanticPredictionErrorEncoder,
     ErrorTemporalStatisticsState,
@@ -22,6 +21,10 @@ from .semantic_recurrent_predictor import (
     SemanticRecurrentPredictor,
     SemanticRestorationHead,
     SemanticDiscrepancyRestorationHead,
+)
+from .task_aware_z4_restoration import (
+    ContextGatedZ4RestorationHead,
+    ErrorRegulatedSemanticRestorationPredictor,
 )
 from .adapters import (
     HOST_STAGE_CHANNELS,
@@ -92,6 +95,7 @@ __all__ = [
     "ErrorGuidedSemanticRestorationPredictor",
     "ErrorRegulatedSemanticStateCell",
     "SemanticDiscrepancyRestorationHead",
+    "ContextGatedZ4RestorationHead",
     "ErrorRegulatedSemanticRestorationPredictor",
     "DecoupledLocalFeatureCorrelation",
     "DecoupledTemporalErrorEncoder",
